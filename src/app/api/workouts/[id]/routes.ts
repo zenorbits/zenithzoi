caa1fdae3ffe25358881201978bd43edd8e40679
ZenOrbits <zenorbits29@gmail.com>
2026-06-06 22:36:06 +0530

    Workout route GET POST DELETE

---
+++ src/app/api/workouts/[id]/routes.ts
@@ -0,0 +1,53 @@
+import { getWorkOutCollection } from "@/lib/collections/workout";
+import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { ObjectId } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const { userId } = await auth();
+    if (!userId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const workouts = await getWorkOutCollection();
+
+    const result = await workouts
+        .find({ userId })
+        .sort({ date: -1 })
+        .toArray();
+
+    return NextResponse.json(result);
+}
+
+export async function POST(req: Request) {
+
+    const { userId } = await auth();
+    if (!userId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const body = await req.json();
+
+    const workouts = await getWorkOutCollection();
+    const result = await workouts.insertOne({
+        userId,
+        date: body.date,
+        exercises: body.exercises,
+        notes: body.notes,
+        createdAt: new Date(),
+    });
+
+    return NextResponse.json({ id: result.insertedId });
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const { userId } = await auth();
+    if (!userId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const workouts = await getWorkOutCollection();
+
+    const result = await workouts.deleteOne({
+
+        _id: new ObjectId(params.id),
+        userId,
+    });
+
+return NextResponse.json({ success: true });
+}
